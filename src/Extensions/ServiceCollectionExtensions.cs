6c8882dbfa2f69c63ae99671ecb3afca680e6c00
AlxndrVgt <alexander.vogt@avolutions.de>
2025-08-18 12:05:11 +0200

    feat: add extension to register baf blazor

---
+++ src/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Avolutions.BAF.Core.Module.Abstractions;
+using Avolutions.BAF.Core.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using MudBlazor.Services;
+
+namespace Avolutions.Baf.Blazor.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddBafBlazor(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        services.AddMudServices();
+        
+        services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+        
+        return services;
+    }
+}
