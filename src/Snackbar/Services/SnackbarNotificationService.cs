52f714cf743c7870ce836f6d6990fcaffa93a22f
AlxndrVgt <alexander.vogt@avolutions.de>
2025-08-28 13:43:52 +0200

    feat: add snackbar module

---
+++ src/Snackbar/Services/SnackbarNotificationService.cs
@@ -0,0 +1,35 @@
+﻿using MudBlazor;
+
+namespace Avolutions.Baf.Blazor.Snackbar.Services;
+
+public class SnackbarNotificationService
+{
+    private readonly ISnackbar _snackbar;
+    private readonly string _position = Defaults.Classes.Position.BottomCenter;
+
+    public SnackbarNotificationService(ISnackbar snackbar)
+    {
+        _snackbar = snackbar;
+    }
+
+    public void ShowSuccess(string message)
+    {
+        _snackbar.Clear();
+        _snackbar.Configuration.PositionClass = _position;
+        _snackbar.Add(message, Severity.Success);
+    }
+
+    public void ShowError(string message)
+    {
+        _snackbar.Clear();
+        _snackbar.Configuration.PositionClass = _position;
+        _snackbar.Add(message, Severity.Error);
+    }
+
+    public void ShowInfo(string message)
+    {
+        _snackbar.Clear();
+        _snackbar.Configuration.PositionClass = _position;
+        _snackbar.Add(message, Severity.Info);
+    }
+}
