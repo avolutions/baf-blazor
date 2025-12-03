882e59c02dc1e79562e8000c12bb23eeb5d148a8
AlxndrVgt <alexander.vogt@avolutions.de>
2025-12-03 17:39:35 +0100

    feat: add visible option to RowAction

--- src/DataTable/Models/RowAction.cs
+++ src/DataTable/Models/RowAction.cs
@@ -14,14 +14,17 @@ public class RowAction<T>
     public Func<T, Size>? SizeFunc { get; set; }
     public string? Text { get; set; }
     public Func<T, string>? TextFunc { get; set; }
+    public bool Visible { get; set; } = true;
+    public Func<T, bool>? VisibleFunc { get; set; }
     public Func<T, Task>? OnClick { get; set; }
 
     public Task InvokeAsync(T item) =>
         OnClick?.Invoke(item) ?? Task.CompletedTask;
     
-    public string GetIcon(T item) => IconFunc?.Invoke(item) ?? Icon ?? "";
     public Color GetColor(T item) => ColorFunc?.Invoke(item) ?? Color;
-    public bool IsDisabled(T item) => DisabledFunc?.Invoke(item) ?? Disabled;
+    public string GetIcon(T item) => IconFunc?.Invoke(item) ?? Icon ?? "";
     public Size GetSize(T item) => SizeFunc?.Invoke(item) ?? Size;
     public string GetText(T item) => TextFunc?.Invoke(item) ?? Text ?? "";
+    public bool IsDisabled(T item) => DisabledFunc?.Invoke(item) ?? Disabled;
+    public bool IsVisible(T item) => VisibleFunc?.Invoke(item) ?? Visible;
 }
