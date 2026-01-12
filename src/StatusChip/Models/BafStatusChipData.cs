8bda9cd663dd1583883b0fd1ff7975bfafd3d439
AlxndrVgt <alexander.vogt@avolutions.de>
2026-01-12 14:28:33 +0100

    feat: add status chip component

---
+++ src/StatusChip/Models/BafStatusChipData.cs
@@ -0,0 +1,5 @@
+﻿using MudBlazor;
+
+namespace Avolutions.Baf.Blazor.StatusChip.Models;
+
+public record BafStatusChipData(Color Color, string Label, string? Icon = null);
---
+++ src/StatusChip/Resources/StatusChipResources.cs
@@ -0,0 +1,6 @@
+﻿namespace Avolutions.Baf.Blazor.StatusChip.Resources;
+
+public class StatusChipResources
+{
+    
+}
