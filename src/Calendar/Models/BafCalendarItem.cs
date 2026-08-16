f5ffe66d45b485f9da73af781f156ed9f7708123
AlxndrVgt <alexander.vogt@avolutions.de>
2026-08-16 16:24:45 +0200

    feat: add calendar component

---
+++ src/Calendar/Models/BafCalendarItem.cs
@@ -0,0 +1,11 @@
+﻿using Heron.MudCalendar;
+using MudBlazor;
+
+namespace Avolutions.Baf.Blazor.Calendar.Models;
+
+public class BafCalendarItem : CalendarItem
+{
+    public string? Description { get; set; }
+    public string? ColorHex { get; set; }
+    public string? Icon { get; set; }
+}
