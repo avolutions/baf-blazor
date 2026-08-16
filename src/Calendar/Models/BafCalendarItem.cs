f587f5fed97d0cc260e059086c18c96d52bf7a53
AlxndrVgt <alexander.vogt@avolutions.de>
2026-08-16 21:49:44 +0200

    feat: improve calendar component

--- src/Calendar/Models/BafCalendarItem.cs
+++ src/Calendar/Models/BafCalendarItem.cs
@@ -1,10 +1,10 @@
 ﻿using Heron.MudCalendar;
-using MudBlazor;
 
 namespace Avolutions.Baf.Blazor.Calendar.Models;
 
 public class BafCalendarItem : CalendarItem
 {
+    public Guid Id { get; set; }
     public string? Description { get; set; }
     public string? ColorHex { get; set; }
     public string? Icon { get; set; }
